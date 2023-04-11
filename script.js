8916396bf41443d1db1403dcbe3685bf56cf4e8a
sachin <sachin270798@gmail.com>
2023-04-11 20:40:43 +0530

    adding two numbers func

---
+++ script.js
@@ -0,0 +1,10 @@
+function add(){
+let a = document.getElementById("first").value;
+let b = document.getElementById("second").value;
+let c=parseInt(a)+parseInt(b);
+document.getElementById("sum").value=String(c);
+// console.log(ans)
+// console.log(c)
+// alert(c)
+
+}
